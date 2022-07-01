19b3306c8f04e7be762f07202c864ca167eb4271
Lais <laismassacesi@gmail.com>
2022-07-01 15:57:07 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isloading: false,
+      redirect: false,
+    };
+  }
+
+  getUserName = () => {
+    const { userName } = this.state;
+    this.setState({
+      isloading: true,
+    }, async () => {
+      await createUser({ name: userName });
+      this.setState({
+        redirect: true,
+      });
+    });
+  }
+
   render() {
+    const { userName, isloading, redirect } = this.state;
     return (
       <div data-testid="page-login">
         <h3>Login</h3>
+        { isloading ? <Loading /> : (
+          <form>
+            <label htmlFor="userName">
+              Nome:
+              <input
+                type="text"
+                data-testid="login-name-input"
+                onChange={ (event) => {
+                  this.setState({
+                    userName: event.target.value,
+                  });
+                } }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ userName.length < +'3' }
+              onClick={ this.getUserName }
+            >
+              Entrar
+            </button>
+          </form>
+        ) }
+        { redirect && <Redirect to="/search" /> }
       </div>
     );
   }
