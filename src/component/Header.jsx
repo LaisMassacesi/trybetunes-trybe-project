da9ae90b435e607a852e2734b8c40e0a89b8ca6f
Lais <laismassacesi@gmail.com>
2022-07-02 13:23:56 -0300

    Requisito 4 e correção Requisito 1

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -25,7 +26,16 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         { loading
-          ? <Loading /> : <header data-testid="header-user-name">{ userName }</header> }
+          ? <Loading /> : (
+            <section>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+              <Link to="/favorites" data-testid="link-to-favorites"> Favorites</Link>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+              <div data-testid="header-user-name">
+                { userName }
+              </div>
+            </section>
+          ) }
       </header>
     );
   }
