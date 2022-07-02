414f3ad02712afb1553b3b89ff2f6925741e3031
Lais <laismassacesi@gmail.com>
2022-07-02 14:18:01 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import Header from '../component/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchArtist: '',
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      searchArtist: event.target.value,
+    });
+  }
+
   render() {
+    const { searchArtist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h3>Search</h3>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ searchArtist }
+            onChange={ this.handleInputChange }
+            name="searchArtist"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ searchArtist.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
