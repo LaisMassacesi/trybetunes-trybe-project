cc08311881303f5ed427dde74419b12843cc3b11
Lais <laismassacesi@gmail.com>
2022-07-22 14:13:17 -0300

    Requisito 1

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -4,7 +4,7 @@ import Header from '../component/Header';
 class ProfileEdit extends React.Component {
   render() {
     return (
-      <div>
+      <div data-testid="page-profile-edit">
         <Header />
         <h3>Profile Edit</h3>
       </div>
